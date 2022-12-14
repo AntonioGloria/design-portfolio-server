bb09213cb9ae299ef3d9ca1b04f75ca186e149f2
Antonio Gloria <angloria87@gmail.com>
2022-12-14 14:47:17 -0600

    updated auth payload to include user bio and tagline fields

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -107,10 +107,10 @@ router.post("/login", (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
-        const { _id, email, username, avatar, coverImg } = foundUser;
+        const { _id, email, username, avatar, coverImg, bio, tagline } = foundUser;
 
         // Create an object that will be set as the token payload
-        const payload = { _id, email, username, avatar, coverImg };
+        const payload = { _id, email, username, avatar, coverImg, bio, tagline };
 
         // Create a JSON Web Token and sign it
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
