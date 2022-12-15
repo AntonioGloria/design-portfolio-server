aa5198f99b85be95977de9997b3348274526c0b5
Antonio Gloria <angloria87@gmail.com>
2022-12-15 11:45:54 -0600

    create artwork backend WIP

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -41,6 +41,19 @@ router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
 
 });
 
+// Upload multiple images
+router.post("/upload-many", fileUploader.array("imageUrl"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  res.json({ fileUrl: req.file.path });
+
+});
+
 // Send new information to update user info
 router.put("/:username/edit-profile", async (req, res, next) => {
   try {
