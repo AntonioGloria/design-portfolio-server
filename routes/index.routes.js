25cf1603ae5dc90dce0e26abab21f4fb460ce4ae
Antonio Gloria <angloria87@gmail.com>
2023-03-08 14:20:47 -0600

    Moved file upload routes from user.routes to index.routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const fileUploader = require("../config/cloudinary.config");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+// Upload image
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  res.json({ fileUrl: req.file.path });
+});
+
+// Upload multiple images
+router.post("/upload-multi", fileUploader.array("imageUrl"), (req, res, next) => {
+  if (!req.files) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  const { files } = req;
+  const urls = files.map(file => {
+    return file.path;
+  });
+
+  res.json({ fileUrls: urls });
 });
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const fileUploader = require("../config/cloudinary.config");
 const User = require("../models/User.model");
 const Album = require("../models/Album.model");
 
@@ -40,34 +39,6 @@ router.get("/:username", async (req, res, next) => {
   catch (err) {
     console.log(err);
   }
-});
-
-// Upload image
-router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
-  if (!req.file) {
-    next(new Error("No file uploaded!"));
-    return;
-  }
-  // Get the URL of the uploaded file and send it as a response.
-  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
-  res.json({ fileUrl: req.file.path });
-
-});
-
-// Upload multiple images
-router.post("/upload-multi", fileUploader.array("imageUrl"), (req, res, next) => {
-  if (!req.files) {
-    next(new Error("No file uploaded!"));
-    return;
-  }
-
-  const { files } = req;
-  const urls = files.map(file => {
-    return file.path;
-  })
-
-  res.json({ fileUrls: urls });
-
 });
 
 // Send new information to update user info
