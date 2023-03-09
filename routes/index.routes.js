fcf98baf35c7132f51dc9d7d158c6c6a97ff9a97
Antonio Gloria <angloria87@gmail.com>
2023-03-09 11:06:01 -0600

    Setup route for deleting Cloudinary image files when projects are deleted

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,28 +7,58 @@ router.get("/", (req, res, next) => {
 });
 
 // Upload image
-router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
-  if (!req.file) {
-    next(new Error("No file uploaded!"));
-    return;
+router.post("/upload", fileUploader.single("imageUrl"), async (req, res, next) => {
+  try {
+    if (!req.file) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
+    res.json({ fileUrl: req.file.path });
   }
-  // Get the URL of the uploaded file and send it as a response.
-  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
-  res.json({ fileUrl: req.file.path });
+
+  catch (err) {
+    console.log(err);
+  }
 });
 
 // Upload multiple images
-router.post("/upload-multi", fileUploader.array("imageUrl"), (req, res, next) => {
-  if (!req.files) {
-    next(new Error("No file uploaded!"));
-    return;
-  }
-  const { files } = req;
-  const urls = files.map(file => {
-    return file.path;
-  });
+router.post("/upload-multi", fileUploader.array("imageUrl"), async (req, res, next) => {
+  try {
+    if (!req.files) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
+    const { files } = req;
+    const urls = files.map(file => {
+      return file.path;
+    });
 
-  res.json({ fileUrls: urls });
+    res.json({ fileUrls: urls });
+  }
+  catch (err) {
+    console.log(err);
+  }
+});
+
+// Delete multiple images
+router.put("/delete-multi/", async (req, res, next) => {
+  try {
+    const { api } = fileUploader.storage.cloudinary;
+    const { folder } = fileUploader.storage.params;
+    const imgUrls = req.body;
+
+    const publicIds = imgUrls.map(imgUrl => {
+      // public_id: "folder_name/file_name" WITHOUT file extension
+      return `${folder}/${imgUrl.split("/").pop().split(".")[0]}`;
+    });
+
+    const deletedFiles = await api.delete_resources(publicIds);
+    res.json(deletedFiles);
+  }
+
+  catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
