6d39790d5b26464c8e4c91892062d07e36d60bff
Antonio Gloria <angloria87@gmail.com>
2023-03-10 17:05:10 -0600

    Added route to create albums

--- routes/albums.routes.js
+++ routes/albums.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User.model");
 const Album = require("../models/Album.model");
 
 // Get all albums
@@ -18,8 +19,20 @@ router.get("/:album", async (req, res, next) => {
   try {
     const { _id } = req.params;
     const albumData = await Album.findOne(_id).populate("artworks");
-    console.log(albumData);
     res.json(albumData);
+  }
+  catch (err) {
+    console.log(err);
+  }
+});
+
+// Create own album
+router.post("/create", async (req, res, next) => {
+  try {
+    const { owner } = req.body;
+    const newAlbum = await Album.create(req.body);
+    await User.updateOne({ _id: owner._id }, { $push : { ownAlbums: newAlbum } }, { new: true });
+    res.json(newAlbum);
   }
   catch (err) {
     console.log(err);
